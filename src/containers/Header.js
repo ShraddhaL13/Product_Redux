158980ab52693ead937dac38751844e9fc829248
shraddh borade <shraddhacareer99@gmail.com>
2023-01-22 23:10:26 +0530

    My first File

---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h1 className="apiName" style={{fontSize:"30px", fontWeight:"bold"}}>FakeShop</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
